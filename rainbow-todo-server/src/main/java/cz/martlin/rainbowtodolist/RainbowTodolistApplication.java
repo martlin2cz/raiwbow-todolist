5379a9c73d08e23ec681b9116adc2db2364dda83
Martin Jašek <martin.jasek@certicon.cz>
2023-08-26 23:56:39 +0200

    Generated the spring service

---
+++ rainbow-todo-server/src/main/java/cz/martlin/rainbowtodolist/RainbowTodolistApplication.java
@@ -0,0 +1,13 @@
+package cz.martlin.rainbowtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowTodolistApplication.class, args);
+	}
+
+}
---
+++ rainbow-todo-server/src/test/java/cz/martlin/rainbowtodolist/RainbowTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.martlin.rainbowtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
