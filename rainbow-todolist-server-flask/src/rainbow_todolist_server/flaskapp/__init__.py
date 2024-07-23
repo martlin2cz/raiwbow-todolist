fcf4d9ed49724532ff49096b474d5eeeda9424c9
martlin <m@rtlin2cz>
2024-07-24 00:27:20 +0200

    The initial CRUD methods for tasks

--- rainbow-todolist-server-flask/src/rainbow_todolist_server/flaskapp/__init__.py
+++ rainbow-todolist-server-flask/src/rainbow_todolist_server/flaskapp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 from ..logic import applogic
 
@@ -13,6 +14,33 @@ def create_app():
     @app.route("/api/v0.2/rainbowtodolist/tasks")
     def list_tasks():
         return applogic.list_tasks()
+
+    @app.put("/api/v0.2/rainbowtodolist/tasks")
+    def add_task():
+        text = request.form["text"]
+        return [ applogic.add_task(text) ]
+
+    @app.route("/api/v0.2/rainbowtodolist/tasks/<uuid:task_id>", methods=['GET', 'PATCH', 'DELETE'])
+    def get_or_modify_task(task_id):
+        if request.method == 'GET':
+            return get_task(task_id)
+        if request.method == 'PATCH':
+            return update_task(task_id)
+        if request.method == 'DELETE':
+            return remove_task(task_id)
+
+    @app.get("/api/v0.2/rainbowtodolist/tasks/get/<uuid:task_id>")
+    def get_task(task_id):
+        return [ applogic.get_task(task_id) ]
+
+    @app.patch("/api/v0.2/rainbowtodolist/tasks/update/<uuid:task_id>")
+    def update_task(task_id):
+        text = request.form["text"]
+        return [ applogic.update_task(task_id, text) ]
+
+    @app.delete("/api/v0.2/rainbowtodolist/tasks/remove/<uuid:task_id>")
+    def remove_task(task_id):
+        return [ applogic.remove_task(task_id) ]
 
     return app
 
