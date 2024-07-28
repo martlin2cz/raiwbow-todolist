b3e438133f1bde42d8f3eb92598ce53ec4d45c44
martlin <m@rtlin2cz>
2024-07-28 21:44:20 +0200

    enable CORS

--- rainbow-todolist-server-flask/src/rainbow_todolist_server/flaskapp/__init__.py
+++ rainbow-todolist-server-flask/src/rainbow_todolist_server/flaskapp/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 
 from ..logic import applogic
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     from .routes import bp
     app.register_blueprint(bp)
